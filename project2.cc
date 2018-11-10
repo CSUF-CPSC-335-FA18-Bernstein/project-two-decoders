48a184472ec8d9491e6eea9e7cf261d83405686b
antonio_lopez <antonio_lopez@csu.fullerton.edu>
2018-11-09 19:52:16 -0800

    added randomize_list and quicksort

--- project2.cc
+++ project2.cc
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -23,13 +24,26 @@ using namespace std;
 // Randomize the order of all items in the list
 //-----------------------------------------------------------------------------
 void randomize_list(string_vector & strings) {
-  // TODO: implement this function, then delete this comment
+	//Don't know if size_t random value goes inside or outside the for-loop
+	//In Berstein's powerpoint slide, it shows it inside, but wouldn't that
+	//loop forever?
+
+	std::srand(std::time(nullptr));		//use current time as seed for random generator
+	//size_t random_value = std::rand();
+	//std::cout << "Random value on [0 " << RAND_MAX << "]: " << random_value << '\n';
+
+	for (size_t i = 0; i < strings.size(); i++) {
+		size_t random_value = std::rand();
+		std::cout << "Random value on [0 " << RAND_MAX << "]: " << random_value << '\n';
+		std::swap(strings[i], strings[random_value]);
+	}
   return;
 }
 
 //-----------------------------------------------------------------------------
 void merge(string_vector & strings, size_t start, size_t mid, size_t end) {
   // TODO: implement this function, then delete this comment
+
   return;
 }
 
@@ -62,7 +76,15 @@ int hoare_partition(string_vector & strings, int start, int end) {
 // the two parts together using the merge() method.
 //-----------------------------------------------------------------------------
 void quicksort(string_vector & strings, int start, int end) {
-  // TODO: implement this function, then delete this comment
+	//needs hoare_partition in order to work
+
+	/*if (start < end)
+	{
+		int split_point = hoare_partition(strings, start, end);
+		quicksort(strings, start, split_point - 1);
+		quicksort(strings, split_point + 1, end);
+	}*/
+
   return;
 }
 
@@ -80,11 +102,11 @@ void quicksort(string_vector & strings, int start, int end) {
 //-----------------------------------------------------------------------------
 bool load_words(string_vector& words, const std::string& path) 
 {
-  //std::cout << "Loading words from [" << path << "]" << std::endl;
+  std::cout << "Loading words from [" << path << "]" << std::endl;		//doesn't work w/o #include<iostream>
   words.clear();
   std::ifstream ifs(path.c_str());
   if (!ifs.is_open() || !ifs.good()) {
-    //cout << "Failed to open [" << path << "]" << std::endl;
+    cout << "Failed to open [" << path << "]" << std::endl;
     return false;
   }
   int countWordsLoaded = 0;
@@ -97,7 +119,9 @@ bool load_words(string_vector& words, const std::string& path)
     words.push_back(lineBuffer);
     countWordsLoaded++;
   }
-  //std::cout << "Loaded " << countWordsLoaded << " words from [" << path << "]" << std::endl;;
+  std::cout << "Loaded " << countWordsLoaded << " words from [" << path << "]" << std::endl;;
+  
+  //randomize_list(words);				//output check to see if function works
   return true;
 }
 
